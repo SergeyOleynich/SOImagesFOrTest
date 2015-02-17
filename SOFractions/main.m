c4f34a0b6f1c4fd4b8cbec91c6b11f09df6be502
Sergey <for_sale@I-hate-you-guys.local>
2015-02-17 12:31:52 +0200

    Initial Commit

---
+++ SOFractions/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SOFractions
+//
+//  Created by Sergey on 17.02.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
