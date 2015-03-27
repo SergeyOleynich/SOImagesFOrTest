82bebd8f3adaf7415f91421b52efefbb5ea985ab
Sergey_Oleynich <sergeyoleynich@gmail.com>
2015-03-27 13:51:44 +0200

    Create

---
+++ SOFractions/SOFraction.h
@@ -0,0 +1,13 @@
+//
+//  SOFraction.h
+//  SOFractions
+//
+//  Created by Sergey on 17.02.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOFraction : NSObject
+
+@end
---
+++ SOFractions/SOFraction.m
@@ -0,0 +1,13 @@
+//
+//  SOFraction.m
+//  SOFractions
+//
+//  Created by Sergey on 17.02.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import "SOFraction.h"
+
+@implementation SOFraction
+
+@end
